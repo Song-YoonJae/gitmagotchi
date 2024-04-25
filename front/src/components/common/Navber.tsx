52e6aada7c976c7004b392aa90aca519ccc2c61f
이유로 <eurohand@naver.com>
2024-04-25 17:16:12 +0900

    ✨ [Feat/168] Character Menu page
    Former-commit-id: 8fc5d9aa103e7fa363b637b85b545d0473e9eac3

--- front/src/components/common/Navber.tsx
+++ front/src/components/common/Navber.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66ed0a7817089e51205cc5c56b3c20f07a03df2a064fa631d32f780e30411c69
-size 4138
+oid sha256:f2b020f6a9e44428039ec1f959ea402b8fe4f1275445d76ef407b462c07b5f81
+size 3910
