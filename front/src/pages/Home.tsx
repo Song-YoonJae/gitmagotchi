64121b03b88d27671361897dea7ff937cef75a6b
이유로 <eurohand@naver.com>
2024-04-25 16:11:18 +0900

    ✍ [Style/168] main servermsg update
    Former-commit-id: 3ffc6c55ad820e498611e4aabf4f394c3d8f9860

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:137f83df890fc8dcf1316a19bb02189305ecf4c2cbcf35fb7e7753756d2258e5
-size 6053
+oid sha256:8d345aa72e44405724f22cd7294000ea26d75ada536c6d4dcc3425bc6e1e055e
+size 6218
