946a945be87dad634a935926912831bf7ac9bdf9
이유로 <eurohand@naver.com>
2024-04-25 15:54:14 +0900

    ✨ [Feat/168] main page responsive
    Former-commit-id: 99c3a118dafa3893e26faaf7fb7e6275d37ff24e

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0098db480ff4ed8616c5ea4aaf0f64c2787f00a2fe3fd257ab342d997b304fcf
-size 4567
+oid sha256:137f83df890fc8dcf1316a19bb02189305ecf4c2cbcf35fb7e7753756d2258e5
+size 6053
