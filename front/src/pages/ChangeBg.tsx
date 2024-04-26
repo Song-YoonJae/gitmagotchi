bd07d1e97c53cb0e9ce2747518b9ff0cb97adc09
이유로 <eurohand@naver.com>
2024-04-26 17:22:22 +0900

    ✨ [Feat/170] changebg page
    Former-commit-id: c678ec00520f17573eaca8d5b0c0fcbf1ac4ad03

--- front/src/pages/ChangeBg.tsx
+++ front/src/pages/ChangeBg.tsx
@@ -1,10 +1,3 @@
-import tw from "tailwind-styled-components";
-
-export default function ChangeBg() {
-  return <Wrapper></Wrapper>;
-}
-
-const Wrapper = tw.div`
-w-full
-h-full
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:c6497ece8930d444ce70074c8479853ac03792a1ddf50f99843986c9cc2065b1
+size 4101
