8ca57361bcdf49ccd987e8f7306207c55d45d832
이유로 <eurohand@naver.com>
2024-04-26 17:22:22 +0900

    ✨ [Feat/170] changebg page
    Former-commit-id: 5966dbd82685155fe024e145b4530e9b1af6197e

--- front/src/pages/ChangeBg.tsx
+++ front/src/pages/ChangeBg.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8db8f3ebfeccced727c2314fa4eee6c749517d823961f43497e72524d2cd4c36
-size 157
+oid sha256:c6497ece8930d444ce70074c8479853ac03792a1ddf50f99843986c9cc2065b1
+size 4101
