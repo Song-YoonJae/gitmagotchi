45366d3bc2b4cc76c2d46f71297c162ec7b116e0
이유로 <eurohand@naver.com>
2024-04-29 17:09:58 +0900

    ✨ [Feat/164] api, animation default setting
    Former-commit-id: 6155a0a09a5bba0fd8992c7a88f61ac9c5ba7170

--- front/src/pages/ChangeBg.tsx
+++ front/src/pages/ChangeBg.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6d43c6326fbcb7c1ba35ff42c1699590c30abc2050cd5d78ded63055be51ba3
-size 4267
+oid sha256:87ba4c2a8a08fc2479daa6e629da56d95f79d8cb763a5df1457411ff1f674226
+size 4288
