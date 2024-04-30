01a9b83b9c4036ea9aa134ebe7eeb47136c9b1f9
이유로 <eurohand@naver.com>
2024-04-30 15:18:50 +0900

    ✨ [Feat/182] add character atom
    Former-commit-id: 849b7aa2e51b8aabc00058b2551a1a1c49dd5d32

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ecfc75452527d194da277b8d3b1d9f5f506c3029ea785aead1d7da748b355f6
+oid sha256:a15fa226406ec9150fee20d1429e9e5ca1bc967d0f46cd828cd227b0a065b4ab
 size 8064
--- front/src/store/atom.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7a19ff035708b7a3a41d1908ad8fe3f4116403792baf87671887113fc1b411f7
-size 493
