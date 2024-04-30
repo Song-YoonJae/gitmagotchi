9078a8b5ecfd22d63b72e725e98146fbcb701957
이유로 <eurohand@naver.com>
2024-04-30 16:12:26 +0900

    ⛏ [Fix/dev-frontend] deploy error fix
    Former-commit-id: 0577b62c5bfcb2cd16666350a8119c94ca92f5be

--- front/src/api/sample.ts
+++ front/src/api/sample.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc4dbee10a70a7698397ee92a3ead58f05cf95a43683e075b099c955f636bb69
-size 10199
+oid sha256:498021eee1f7718b6e4a384cef8acd2a13247facfc7955444833be972752863a
+size 10064
--- front/src/pages/Search.tsx
+++ front/src/pages/Search.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a52908c112e380701a554b49b6b953dd6cf0d546cbf4e2f6a637b0722f460700
-size 5394
+oid sha256:cda96724fb445efbb67e994771ce4091ee432ab20ccd87b65fa8c8bd40ddcab1
+size 5550
