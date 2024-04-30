f44cfa27a664696dacd9b5c2b64a1eaf2c04237d
이유로 <eurohand@naver.com>
2024-04-30 09:17:58 +0900

    ⛏ [Fix test/dev-frontend] mobile web hide address bar (test)
    Former-commit-id: 9422fe9666a10df175c3d2aa3770eac02ccacd82

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dae2a652b81dde56360e9bb2afc9c181ba41e1b12663a1608afb837b6d3427eb
-size 329
+oid sha256:92588c537d007bf50d2c95c01e620e19c7761998aef712d070c2320995a8bd6b
+size 449
