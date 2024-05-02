e01747bf1f4d645359a1c846636ac774e7302ffe
권송아 <gunsong2@naver.com>
2024-05-02 09:14:33 +0900

    ✨ [Feat/143] Make a docker image for motions
    Make a docker image for motions
    
    
    Former-commit-id: bacaa0d6159a58563e2a820bff5576d5db4d5fda

--- back/python/motion/AnimatedDrawings/animated_drawings/view/window_view.py
+++ back/python/motion/AnimatedDrawings/animated_drawings/view/window_view.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:053b87f3aabf378a419c8a015fbbdcdc3983d1591478449ad645da92ad979eb2
-size 6993
+oid sha256:fdaee887b47a2e5a31dc644089de5ab197ed731bc0b2314bf13e39f6dbc3d44f
+size 7041
