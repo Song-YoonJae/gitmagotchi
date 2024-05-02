7584ddf76387911d8bd9668981927f8157190105
이유로 <eurohand@naver.com>
2024-05-02 17:42:22 +0900

    ✨ [Feat/239] generate bg api work
    Former-commit-id: d80be9c7edd3dff79de95f1d8cdff61f4cac9fd0

--- front/src/components/createBg/CreateByAi.tsx
+++ front/src/components/createBg/CreateByAi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:beb72ad7d48bb18fcb6caee171e3d948cf641c2dff1a76e5c508d29f6dcaefc2
-size 2244
+oid sha256:1b4c1b63821a9680ed0bcc0f7dc1d40e771e650de811eac45e227b0528da669c
+size 2327
