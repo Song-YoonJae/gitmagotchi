1ef55528d2e86f532e6f1e9f8680a04a75368677
이유로 <eurohand@naver.com>
2024-05-03 17:27:40 +0900

    ✨ [Feat/239] snake game
    Former-commit-id: cab02f40b54a9e1a151e494d583041b0a2886c43

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3ea43a2dbf35bb863ad986f5b204d928b28dcff0c4cfca51f13520f59e7d440
-size 2782
+oid sha256:b57681c89e88816ad380f90095116f95b70c05fbcf7cc11acab9776c1180463a
+size 2891
--- front/src/pages/Ranking.tsx
+++ front/src/pages/Ranking.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cf7fa0495e3b76c71566935503e8b766bb12779941184d143d53b4ca34c4b6e
-size 2837
+oid sha256:d8f72b73b8af3b1a71240a1e20409b6d7a0af720f25fceaf76b47286adc43999
+size 2879
