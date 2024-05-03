8c857990a41830fddc667912b1ef450c6ed3bff0
이유로 <eurohand@naver.com>
2024-05-03 10:27:39 +0900

    ✨ [Feat/239] cconnect create bg api
    Former-commit-id: b0c70ad5cb244de75010244436816ff143d1da66

--- front/src/pages/ChangeBg.tsx
+++ front/src/pages/ChangeBg.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f35611820bbb7ffff4c311dd52a2bcbd0e0df6b875adbd95080472b899081b9f
+oid sha256:4f278df8ebfb944b7113a37747787d3980aad1c4190d48cb013ad877ad38254f
 size 3378
