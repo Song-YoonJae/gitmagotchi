0aa2bb961cc95933c1f7af19bf310e6486f48f12
이유로 <eurohand@naver.com>
2024-05-03 17:56:53 +0900

    ⛏ [Fix/239] snake game fix
    Former-commit-id: 52eb079f78435a63b42cb1432fd3a9bd2518449a

--- front/src/components/common/CommonInput.tsx
+++ front/src/components/common/CommonInput.tsx
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b06803fd7d0c9d75e62233e760952a51945a26f3be45a368d41209343f45b3bd
-size 350
+import tw from "tailwind-styled-components";
+
+interface IProps {
+  props: object;
+}
+
+export default function CommonInput({ props }: IProps) {
+  return <Input {...props} autoComplete="off" />;
+}
+
+const Input = tw.input`
+w-full
+h-10
+rounded-lg
+shadow-lg
+px-6
+border
+text-xl
+border-slate-400
+bg-slate-300/30
+text-slate-500
+placeholder:text-slate-400
+`;
--- front/src/pages/Minigame.tsx
+++ front/src/pages/Minigame.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:320e49b70d81699c69efcc28e82d163349568a732d1b142ab3515a80defefb6d
-size 7782
+oid sha256:2a76b506c7daadae2f1e61b94ff29f2e5786300245e34166abd77a16e7ab6d57
+size 8100
