4106f3082d283eb9d9730239970290dbf04883a0
권송아 <gunsong2@naver.com>
2024-05-05 17:45:11 +0900

    ✨ [Feat/224] Added code for replacing face with a new image
    Added code for replacing face with a new image
    
    
    Former-commit-id: ac6462d02ec09fa99ca15d58dbb395c42c9b88ea

--- back/python/motion/AnimatedDrawings/animated_drawings/controller/video_render_controller.py
+++ back/python/motion/AnimatedDrawings/animated_drawings/controller/video_render_controller.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24ae3b4e6893aafd0f6e3fb83fac4747d358a5958b121ea7e245ee9af07de406
-size 10251
+oid sha256:25537e527b91fad16cfbe34e33f7b252d62fae2ac7d578cfae0bbacd0c8fe26a
+size 10250
--- back/python/motion/make_templates.py
+++ back/python/motion/make_templates.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e5f6767eb1d0bd858d73b738525c88c68e81abc4a183ddcebd16c22722f2501
-size 981
+oid sha256:0a7027d6901e0d4691d7c55b76b6aa8bcef073244c6d7525f3522a20d8a37494
+size 975
