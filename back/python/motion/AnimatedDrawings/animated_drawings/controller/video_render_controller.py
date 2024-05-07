811823171cc2bc02005cac3b3fcd3d0bdb59bfa3
권송아 <gunsong2@naver.com>
2024-05-07 10:37:41 +0900

    ✨ [Feat/224] Added new motions
    Added new motions
    
    
    Former-commit-id: b698aae03fbbb11a3d86a9266ea7927c3c679a86

--- back/python/motion/AnimatedDrawings/animated_drawings/controller/video_render_controller.py
+++ back/python/motion/AnimatedDrawings/animated_drawings/controller/video_render_controller.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25537e527b91fad16cfbe34e33f7b252d62fae2ac7d578cfae0bbacd0c8fe26a
-size 10250
+oid sha256:651dd36c27845d4e61dc0f9d01fc6163fffa89dad37812d5a2385b9f7801c08d
+size 10190
