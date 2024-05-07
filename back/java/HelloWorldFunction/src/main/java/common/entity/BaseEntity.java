67f757ace9303bc7a51489496877f1bac9f8cc57
heeyeon <kho200a@naver.com>
2024-05-07 16:47:21 +0900

    ✨ [Feat/252] user 두ãID 조회 로직 구현
    Former-commit-id: 74545d62bfbb46f333ed6498f77784e6637f9ab3

--- back/java/HelloWorldFunction/src/main/java/common/entity/BaseEntity.java
+++ back/java/HelloWorldFunction/src/main/java/common/entity/BaseEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53b50ea68cb8cb34ef8cac6c8c9c00a57ad2dea6f718bff3b070c3cdad7775a2
-size 840
+oid sha256:13b658fe32e6ae9d6ba3923e141e5b957ec7e0197041e39b4ee2bc3f500f6a9b
+size 971
