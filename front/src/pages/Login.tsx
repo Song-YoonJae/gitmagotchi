9082761f151b68dd8e33c123c5f3d256843201d8
이유로 <eurohand@naver.com>
2024-05-07 16:25:39 +0900

    ⛏ [Fix/dev-frontend] login error fix
    Former-commit-id: 79a7cf73054a294db1451c280dc509ce85358458

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45c7cc4464b7eb7b9f62c58c43010b7ca214b37d7d16c35d9965c506c672cd7e
-size 1952
+oid sha256:1ac21bbebb2ab38127b8d3113537efa3437cc9d4f2c043a5e1fcc621491a87b8
+size 1436
