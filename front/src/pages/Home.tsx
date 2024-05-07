254917df653d7b18f0527330c9721755db7c3b40
이유로 <eurohand@naver.com>
2024-05-07 17:05:02 +0900

    ✨ [Feat/dev-frontend] face in minigame
    Former-commit-id: f112052785c4fe4f7b6b49f56ec36265d2a8bc13

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93aa08fd75a276cdb59fdcec2a23338051d2d599b3fa55d7b3e1239b60f67bef
-size 11047
+oid sha256:c7da21f2515e5aebf53c5ea27acc2dbf5356cdb410542bcf73baeca77a44ba14
+size 11107
--- front/src/pages/Minigame.tsx
+++ front/src/pages/Minigame.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7702f1f069c4a15f4f8e99de67a7d329112cffa16d3c105f4d629f4425964c43
-size 7942
+oid sha256:6a8b5f879c4f29e3dab3ba988dc7ae03124f6cf85a3d0540b0aab9aaaabae7f8
+size 8875
