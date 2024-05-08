03085d3549ee0467b1d1b1e1651dc39e84fc0579
권송아 <gunsong2@naver.com>
2024-05-09 07:21:04 +0900

    🗃️ [Style/269] Changed a response body
    Changed a respone body
    
    
    Former-commit-id: 0c2bb8f6b62ddfb1e9f181d74e127c199c49607b

--- back/python/motion/get_character_img.py
+++ back/python/motion/get_character_img.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f69e630708915b176c6f5404d95ff7adc8b7cd0afb4db69692e51ab044cca88d
-size 2194
+oid sha256:b3316638f9f8329b1f63bf4c75f1e19179773850b5578d376bfca94f87d480c4
+size 2359
