e8aaa03d3962719a83cf17721beeae8ba39f8111
이유로 <eurohand@naver.com>
2024-05-08 10:31:23 +0900

    ✨ [Feat/218] animation modal
    Former-commit-id: 0c52d986236e4f2355ec340de968f56c4142b133

--- front/src/pages/MyPage.tsx
+++ front/src/pages/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022bca03ea272aebce5c4e2eaee4beda82a1608c26abe6f516054b8f09439429
-size 2737
+oid sha256:57ada17b1875b81105461fa21b6cb85d17487fa34b05ec470f09554ee7dc446b
+size 2787
