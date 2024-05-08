39fb5d88245db14fa4bba6f4cf623fcf103973dd
heeyeon <kho200a@naver.com>
2024-05-08 16:35:26 +0900

    ✨ [Feat/253] collection 명예의 전당 조회
    Former-commit-id: 3b6ad2968fab18b682de8d302e0a398f6487befb

--- back/java/HelloWorldFunction/src/main/java/collection/handler/CollectionHandler.java
+++ back/java/HelloWorldFunction/src/main/java/collection/handler/CollectionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fce0019cdaa5b73de6e0d89337b05445e60dedae9cd4773fb1978d487572d76c
-size 4367
+oid sha256:477c3c48310c067e684e1e7e5a618e1258ec7d7bb8d675c139517609f0e63719
+size 4241
