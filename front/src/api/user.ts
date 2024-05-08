bda4c650a4aa36e526c2f68b4989393fd43c8cb3
이유로 <eurohand@naver.com>
2024-05-08 15:54:16 +0900

    ✨ [Feat/218] edit profile page
    Former-commit-id: fac0a994cb0fbfa452c2daf41b231186847498cb

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -1,9 +1,3 @@
-import { sampleUser } from "./sample";
-
-export const getUser = async (): Promise<any> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(sampleUser);
-    }, 2000);
-  });
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:36c620f6aad2a4d02142351c7a499606ecc63c8e85a764f6402f1e7042b9919b
+size 381
