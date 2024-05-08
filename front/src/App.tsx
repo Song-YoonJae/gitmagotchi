45a1e3207e0643fe32e38b0a699312854e30549e
이유로 <eurohand@naver.com>
2024-05-08 16:12:47 +0900

    ⛏ [Fix/dev-frontend] deploy fix
    Former-commit-id: 005b32692919ce0c3bc45801d6e75c41b03672bf

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78c137c3bd2260aea51e3e84f4e5f05e6f6e7be64e7d41083f685b3693fc1aad
-size 4986
+oid sha256:b710ecdde7cc13b62f63d3b5a90887d879eb45b1ff0b61a2ef8c9576a8da2fc9
+size 4953
