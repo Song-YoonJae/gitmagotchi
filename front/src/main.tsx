1b215cef91fddfd627bece9d7f077f9d44a4469f
송윤재 <1218song@naver.com>
2024-05-08 12:03:23 +0900

    ⛏ [Fix/219] fix prompt
    Former-commit-id: b61ebbd41d6d9126bce01fbf4adf906f89dadba7

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:abe0edc1f91f5d414bbfb8f335f8871b4bf4edf07a861ab225e368a8319cdb6b
-size 236
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
