730d703d51ccb4c13f485bbb9b0d3495770a1e14
KohSulim <sulim0314@naver.com>
2024-05-08 13:34:23 +0900

    ⛏ [Fix/220] signin-signup 람다 수정
    Former-commit-id: 936b994c8e83d5942bc27d7ac321142ad524e843

--- back/sst/api-oauth-github/packages/functions/src/user.ts
+++ back/sst/api-oauth-github/packages/functions/src/user.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0026701a1e302fa675a7f5d1915c4d5f11ccf73af797e6cec891be9a9bfc43e8
-size 1690
+oid sha256:7afb42c5247c8d14bb55df597da1ea4f5e4de216571a58f3a597f686e05e163e
+size 1678
