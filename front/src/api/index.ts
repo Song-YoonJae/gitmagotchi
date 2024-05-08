246568278da8535410d381383009628b641c90b3
이유로 <eurohand@naver.com>
2024-05-08 10:12:59 +0900

    ⛏ [Fix/218] logout fix
    Former-commit-id: 9c5d0533bdb5315281947fdf1ee702b537886f5d

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bebd4ac92ba1795d9b6cd651fcaef4197b84746dc6505351ca2a853a1e8468c7
-size 812
+oid sha256:564be3baa1bab2daf89bf167300ceff431f68e6530590b6c3175813a2ebbb7bb
+size 1430
