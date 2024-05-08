c92e8c6596c6a00998544d11c3a6a599ebeb562a
권송아 <gunsong2@naver.com>
2024-05-08 17:59:59 +0900

    ✨ [Feat/221] Create a motion POST API
    Create a motion POST API
    
    
    Former-commit-id: 0fecf02a6ea76a4392451e48dc34ba600b57ab51

--- back/python/motion/AnimatedDrawings/gitmagotchi/motion/image_to_animation.py
+++ back/python/motion/AnimatedDrawings/gitmagotchi/motion/image_to_animation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d865360966017a7ef7ead0bf7c35e314a512c96dce3fa3460d26b582e9972a7
-size 495
+oid sha256:39304610610e8f9a6715105bfd3841d9faa1a23a6e5d728b79605d670a6dd1de
+size 2818
