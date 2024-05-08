e67a9e80a2942174fbc6f997ef1944e34929b9ff
이유로 <eurohand@naver.com>
2024-05-08 14:38:31 +0900

    ✨ [Feat/218] hungry effect
    Former-commit-id: 4509a7f465049cfb3bac34ad59c9bbb66095c630

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6f5dca302682110c101ef1616e6a6af5f97c03849491989b7ab815342886e7d
-size 14008
+oid sha256:fa664d33a6b544dabe881cc7f7b24407378f5ccb6ceffe552d2e7311ab51f7c9
+size 14249
