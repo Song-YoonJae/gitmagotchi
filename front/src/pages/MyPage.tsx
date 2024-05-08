d82bad9b307485713c6290a0bbb2befc277dd39f
이유로 <eurohand@naver.com>
2024-05-08 10:12:59 +0900

    ⛏ [Fix/218] logout fix
    Former-commit-id: 345bce869dde84c35d56ae8d04229b41c5540a48

--- front/src/pages/MyPage.tsx
+++ front/src/pages/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbb6790be7047cbc4536876ae35bb203e05ed88132bcaffe9e1fb321064354f0
-size 2843
+oid sha256:022bca03ea272aebce5c4e2eaee4beda82a1608c26abe6f516054b8f09439429
+size 2737
