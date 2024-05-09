22375cdc647a26abab2a512ad9dc5bb0b8c4f265
heeyeon <kho200a@naver.com>
2024-05-09 17:01:35 +0900

    ⛏ [Fix/292] size, page 빈문자열 처리
    Former-commit-id: 735649eb2d941e666933ea0389d6e56f264c30f8

--- back/java/HelloWorldFunction/src/main/java/collection/handler/CollectionHandler.java
+++ back/java/HelloWorldFunction/src/main/java/collection/handler/CollectionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0047808a337bcae16a16e322f36265f383ceb04b4daceed7c00b896a8e4d0333
-size 6445
+oid sha256:64577031dfe6f5520ee693daa93f67e73d7efafa194b16da98caa23e74d55c62
+size 6671
