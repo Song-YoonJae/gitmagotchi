acaed59e64480db4f17b41734820be17b0732c0b
이유로 <eurohand@naver.com>
2024-05-09 09:13:37 +0900

    ⛏ [Fix/dev-frontend] fetch user fix
    Former-commit-id: 2f7e4662e812260a2c1a72f11148abd1927464d4

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ffb11a863eb1c1b222a6444d8be558fce63aeb104813c0217376df78ae3e730
-size 5065
+oid sha256:b340b622f0bd64122030ad746fd80824cac9fcdf052ab0bdc9a075ce4ec974a0
+size 5354
