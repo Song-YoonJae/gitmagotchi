417fdfb3de41106e6fa2d17883eed06a98ca8cce
이유로 <eurohand@naver.com>
2024-05-09 11:03:20 +0900

    ⛏ [Fix/dev-frontend] polling fix
    Former-commit-id: b53dd4b2cf4eab90a909127e9ca3b9b77fbfeef5

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -1,13 +1,3 @@
-import { seoulInstance } from ".";
-// import { sampleUser } from "./sample";
-
-export const getUser = async (params: any): Promise<any> => {
-  const response = await seoulInstance.get("/users/me", { params });
-  console.log(response.data);
-  return response.data;
-  // return new Promise((resolve) => {
-  //   setTimeout(() => {
-  //     resolve(sampleUser);
-  //   }, 2000);
-  // });
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:60aaeb9f168dc20d6e19c54dd00ee534fe1e64e31e60b54baa3d7073cb2d014b
+size 357
