827b8add212bccea9d7f74c7913a9f151148e97f
이유로 <eurohand@naver.com>
2024-05-09 11:03:20 +0900

    ⛏ [Fix/dev-frontend] polling fix
    Former-commit-id: 6012da9b01a7bfed5444bf13b2f61cb9eb4b7278

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b340b622f0bd64122030ad746fd80824cac9fcdf052ab0bdc9a075ce4ec974a0
-size 5354
+oid sha256:7cbe77caa769f77dc55ffc5303af22435f1015a1c3591a427bfaf7fe69d94983
+size 5358
