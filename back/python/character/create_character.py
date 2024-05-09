efa4f323348a4c47bbcd558b1dd25af492caa690
송윤재 <1218song@naver.com>
2024-05-09 13:56:32 +0900

    ⛏ [Fix/224] Create stat and status when creating characters
    Former-commit-id: 401a7dd81cfa71625354012c2a4f9ff0009b8cd7

--- back/python/character/create_character.py
+++ back/python/character/create_character.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db3eb05546d46d4457e9aaaa89d15b396887a92de872c2d4979a57b0ae49df8f
-size 2459
+oid sha256:bbb68d3e8177deca222c3787d5765ae9ae8c3b01ff77a95c716e129b2e39656a
+size 3204
