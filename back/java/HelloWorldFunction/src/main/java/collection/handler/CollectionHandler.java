39f7b18d672d9c97382cc1676aa9b5f94f0d73c3
heeyeon <kho200a@naver.com>
2024-05-10 16:03:46 +0900

    ⛏ [Fix/270] search-collection userId 타입 수정 & user 엔티티 type 수정
    Former-commit-id: c2f210410d809cc4fad7fac1ef9b2a5f10cfab6c

--- back/java/HelloWorldFunction/src/main/java/collection/handler/CollectionHandler.java
+++ back/java/HelloWorldFunction/src/main/java/collection/handler/CollectionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ee47f154ab48e0b998e119ec3163a1be5726f1daaec8315836dbce4baf8b518
-size 6675
+oid sha256:f3b8f7ba9cdfda26a8d8db3e5e2c450d15aa3cb7522e3ea1defd90e5de254a99
+size 6683
--- back/java/HelloWorldFunction/src/main/java/user/handler/LankHandler.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7b078dd4cf982751d455fa870ecfff8b32b1bb11c9dcb9396626ecd6b5ca795f
-size 5051
