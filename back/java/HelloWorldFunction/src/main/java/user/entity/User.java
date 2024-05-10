b7d2f5ae5b05389451d1dede3243cd44a9432997
heeyeon <kho200a@naver.com>
2024-05-10 16:03:46 +0900

    ⛏ [Fix/270] search-collection userId 타입 수정 & user 엔티티 type 수정
    Former-commit-id: c50fcba45de5659b3221c9b743676e8823550c83

--- back/java/HelloWorldFunction/src/main/java/user/entity/User.java
+++ back/java/HelloWorldFunction/src/main/java/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60272209c3edc9003f88c7639d69a93fa641df2fc8ac59a4efa89195ab7c55b9
-size 1153
+oid sha256:c18f8a121b91eee646cee367e973049b9907807d1d044719743a52768125d56a
+size 1149
