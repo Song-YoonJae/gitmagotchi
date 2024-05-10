957fd6b85297ad40cb9388a957705519d4ebd047
heeyeon <kho200a@naver.com>
2024-05-10 14:28:55 +0900

    ✨ [Feat/270] 랭킹 기능 구현
    Former-commit-id: 53ca45d138bf37400557fae0a4c2fd08daed4704

--- back/java/HelloWorldFunction/src/main/java/user/entity/User.java
+++ back/java/HelloWorldFunction/src/main/java/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:097a7259b4ac27f5d65929575c0f00d708d8c59956873819cd64682cd32dbfeb
-size 1118
+oid sha256:17f26826ea244d88aadbe297210b568661755ee543d019970afd8e94feebf044
+size 1149
