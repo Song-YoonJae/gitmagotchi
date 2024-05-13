471f9e152a3dd4734225a0de8c844de363091582
heeyeon <kho200a@naver.com>
2024-05-13 17:32:24 +0900

    ⛏ [Fix/295] refactor
    Former-commit-id: 985e43bf21d1d81b787566c4dede8f68abd3e709

--- back/java/HelloWorldFunction/src/main/java/user/handler/RankMeHandler.java
+++ back/java/HelloWorldFunction/src/main/java/user/handler/RankMeHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:043a674b3fe1cc8ea695676c016153aa9ba4933c8e2d85a020cb9ad6e3c16b2f
-size 3715
+oid sha256:74354ec3868ac66e6647ba52d3589432e5a3624656caf20b839a567c22d2ab21
+size 3667
