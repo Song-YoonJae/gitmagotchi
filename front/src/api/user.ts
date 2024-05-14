ba7973f965dee51e81e76a3f03aee8c35c702067
heeyeon <kho200a@naver.com>
2024-05-14 11:10:26 +0900

    ⛏ [Fix/294] RankMeHandler userId 연동
    Former-commit-id: 9ac6792424d576aee573d699c3149bc92bced50f

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:60aaeb9f168dc20d6e19c54dd00ee534fe1e64e31e60b54baa3d7073cb2d014b
-size 357
+import { seoulInstance } from ".";
+// import { sampleUser } from "./sample";
+
+export const getUser = async (params: any): Promise<any> => {
+  const response = await seoulInstance.get("/users/me", { params });
+  return response.data;
+  // return new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve(sampleUser);
+  //   }, 2000);
+  // });
+};
