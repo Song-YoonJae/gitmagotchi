a159ed2bcadbbd890e0b84ba23d8bb182fd197f6
권송아 <gunsong2@naver.com>
2024-05-14 14:32:34 +0900

    ✨ [Feat/227] Added new motions
    Added new motions
    
    
    Former-commit-id: 5c67f912a600d79e527eec927292720cae03b4a1

--- back/sst/api-oauth-github/packages/frontend/src/sst-env.d.ts
+++ back/sst/api-oauth-github/packages/frontend/src/sst-env.d.ts
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f119c9b14c7663be3d4510e5ff1149f92bc461f8e45e598b2d50f09cc38db4c
-size 402
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_COGNITO_DOMAIN: string
+  readonly VITE_APP_STAGE: string
+  readonly VITE_APP_API_URL: string
+  readonly VITE_APP_REGION: string
+  readonly VITE_APP_USER_POOL_ID: string
+  readonly VITE_APP_IDENTITY_POOL_ID: string
+  readonly VITE_APP_USER_POOL_CLIENT_ID: string
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
