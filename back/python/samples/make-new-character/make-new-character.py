552a0fa7be58eb0df0466f44ba222b987c89816c
권송아 <gunsong2@naver.com>
2024-05-16 20:14:16 +0900

    ♻️ [Refactor/271] Added a sample code for new characters
    Added a sample code for new characters
    
    
    Former-commit-id: 789c205c3876ad64c2f3fad75f308202b6debc97

--- back/python/samples/make-new-character/make-new-character.py
+++ back/python/samples/make-new-character/make-new-character.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c54411ed14693e8ba2bb80411277913e88e583c0754efa8bef1b448d0bd632e3
-size 3539
+oid sha256:f8477e693bfb949b0119965527fa85ede6302ce3d0c07b699f187f2b28369db7
+size 3530
