d5e913cfeea26e851534eec54b8e98ea104c13ba
이유로 <eurohand@naver.com>
2024-05-16 11:04:07 +0900

    ⛏ [Fix/dev-frontend] background error fix
    Former-commit-id: 1499f3ec4692ee8180d9ca7f16eede024a31ee61

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0f0aa5c3601654614263859e2597be845973ad6deeb1f84ffdb2e52f8432462
-size 7350
+oid sha256:7494d75e0cd9e9dba09f81956ed5d8bc800a797808e9ff457b1b3efa39bd1968
+size 7403
