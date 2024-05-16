a9cf0a2f0ac6f2e400829699c63d525f83f56312
권송아 <gunsong2@naver.com>
2024-05-16 20:09:49 +0900

    🔨 [Fix/271] Updated a code to make a new character
    Updated a code to make a new character
    
    
    Former-commit-id: 6ae8fa44a3c87dd6ac7c65e57d6516809aea073b

--- back/python/motion/assign_character_motion.py
+++ back/python/motion/assign_character_motion.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77c87e4aec6baae5a978e0e460716fd4c1751ed7cd1009d166199dd374a8e925
-size 4357
+oid sha256:b0c4038104b2814197a82436bfdad9dbc7b4f6d93ca67875a6bb0936be4da9bf
+size 4655
