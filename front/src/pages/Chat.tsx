b1007742ee45622ad1b7dd2daaf37bf23ae0bb68
이유로 <eurohand@naver.com>
2024-05-18 03:10:11 +0900

    ✨ [Feat/dev-frontend] animation update
    Former-commit-id: 627bf5f1db6b454860136c8b798a5f83d1a10c51

--- front/src/pages/Chat.tsx
+++ front/src/pages/Chat.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16dca3b16abb38e2cedaa34362e36624a1b5b53ae9a6a409b8217d4753a2c67f
-size 7714
+oid sha256:3e513b567af54dde93821d618cf45ae57c4976ba07e12ee08993b1c15e272ac0
+size 7750
