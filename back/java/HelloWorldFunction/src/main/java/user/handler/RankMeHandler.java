b2b74f506717dc521afcdf21c9eb00fbee5c5914
heeyeon3050 <kho200a@naver.com>
2024-05-17 12:36:25 +0900

    ⛏ [Fix/302] CORS 코드 추가
    Former-commit-id: ed765333f2f6119d698a7ef9f4cd73d4b11333d2

--- back/java/HelloWorldFunction/src/main/java/user/handler/RankMeHandler.java
+++ back/java/HelloWorldFunction/src/main/java/user/handler/RankMeHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b30f24a7dde5005e2d068b7d656494574de522b5c3418e19ef83c9a6c7a38935
-size 4536
+oid sha256:91265cfd78a39ed11b1ee0527d5254ccc9e9af980d9fa80f4adb3e2b7418e204
+size 4605
