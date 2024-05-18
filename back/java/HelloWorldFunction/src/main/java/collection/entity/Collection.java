2f4043ca1fc155cb64fcc5b8a96fcbbd81b651f7
heeyeon3050 <kho200a@naver.com>
2024-05-18 12:37:52 +0900

    ⛏ [Fix/303] backgroundUrl 칼럼 추가
    Former-commit-id: 4260eaa3ba9d143eb430efb1b4fdbde8a169a1f2

--- back/java/HelloWorldFunction/src/main/java/collection/entity/Collection.java
+++ back/java/HelloWorldFunction/src/main/java/collection/entity/Collection.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88046be9dd59c462f980c739905a8531802199271ec89e349ee5af77dec9771f
-size 906
+oid sha256:7e35f4cb5a47afaa46684e0f5666f2a62b30780213c9f599759ef96eba4581de
+size 977
