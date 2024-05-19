ff8bd78082056f60f81d706639e3bc405d345613
이유로 <eurohand@naver.com>
2024-05-20 00:52:02 +0900

    ✨ [Feat/dev-frontend] character ending update
    Former-commit-id: fc074d6bf67ae8d7ec3d715728a51bc9fd39e36c

--- front/src/pages/CharacterEnding.tsx
+++ front/src/pages/CharacterEnding.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c938ee717ba561b8a3468e699bbf8de48825db1a54f86cc36ed6f3c6d0c74f3
-size 3623
+oid sha256:36046e3d257c1e1c2a7137b9f0909de70e2ba811a435bd1437074f72546a09bb
+size 3615
