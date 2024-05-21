4ac78e8f669958fa4069997e0d425ad7f6f92f5d
이유로 <eurohand@naver.com>
2024-05-21 21:01:02 +0900

    ✨ [Feat/dev-frontend] search update
    Former-commit-id: 5f94b984452302ed5dca3e5eff2470ea0cb732e8

--- front/src/pages/Search.tsx
+++ front/src/pages/Search.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7427b0a7fe7a26cdfc884e3536edbd5507c70c0b08f5c853fc50a1cfb5afb86a
-size 6803
+oid sha256:1416903b12d7c30d562cc948a9578f137e9af39977ff16e698956db734c0fbd2
+size 6864
